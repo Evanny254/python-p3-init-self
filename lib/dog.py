cd66ae882c930f5a0f73096553917e739c84033a
Evanny254 <evancherry254@gmail.com>
2023-11-28 13:40:31 +0300

    Completed Assignment

--- lib/dog.py
+++ lib/dog.py
@@ -2,7 +2,7 @@
 
 class Dog:
     pass
-    def __init__ (self, name, breed=None):
-     self.name = name
-     self.breed = breed if breed else "Mutt"
+    def __init__(self, name, breed=None):
+        self.name = name
+        self.breed = breed if breed else "Mutt"
     
